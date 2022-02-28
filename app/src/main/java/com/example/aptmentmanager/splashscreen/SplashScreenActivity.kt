78914caebe4d636c0c12d5ef09cb67726e3efec6
brsbello <Br!753159>
2022-02-28 18:52:30 -0300

    SplashScreen implementada

---
+++ app/src/main/java/com/example/aptmentmanager/splashscreen/SplashScreenActivity.kt
@@ -0,0 +1,30 @@
+package com.example.aptmentmanager.splashscreen
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.aptmentmanager.MainActivity
+import com.example.aptmentmanager.databinding.ActivitySplashScreenBinding
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivitySplashScreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setup()
+    }
+
+    private fun setup() {
+        binding.ivBuilding.alpha = 0f
+        binding.ivBuilding.animate().setDuration(1700).alpha(1f).withEndAction {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(
+                androidx.appcompat.R.anim.abc_fade_in, androidx.appcompat.R.anim.abc_fade_out)
+            finish()
+        }
+    }
+}
